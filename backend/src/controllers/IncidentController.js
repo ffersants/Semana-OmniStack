2cd8cfa9eb5fc297b635107a2583e58d4024a97c
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-01-30 22:04:47 -0300

    Backend finalizado.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -38,11 +38,18 @@ module.exports = {
         const { page = 1 } = request.query;
 
         const [count] = await connection('incidents').count()
-        console.log(count)
         const incidents = await connection('incidents').select('*')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
         //passa o total de incidents
         response.header('X-Total-Count', count['count(*)'])
         return response.json({ incidents })
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 //importa as funcionalidades do micro framework express
 const express = require(`express`);
 const routes = require('./routes')
-
+const cors = require('cors')
 //instancia a aplicação
 const app = express();
+//permite que todas aplicações possam fazer requisições a esse backend
+app.use(cors());
 
+/*
+QUANDO FOR PARA PRODUÇÃO
+    app.use(cors({
+        origin: 'http://meu-front-end.com'
+    }));
+*/
 //formata o corpo das requisições em objeto JSON entendível pelo javascript
 app.use(express.json())
 app.use(routes)
